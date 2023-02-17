7805d2c7dd168f068c5902babbed74dd6193a48b
suraj0901 <66919989+suraj0901@users.noreply.github.com>
2023-02-17 11:50:22 +0530

    2 files modified

--- public/app.js
+++ public/app.js
@@ -1,1 +1,68 @@
-{}
+{
+  "html": {
+    "type": "Element",
+    "name": "div",
+    "attributes": [],
+    "children": [
+      {
+        "type": "Text",
+        "value": ""
+      },
+      {
+        "type": "Element",
+        "name": "button",
+        "attributes": [
+          {
+            "type": "Attribute",
+            "name": "on:click",
+            "value": {
+              "type": "Expression",
+              "value": "increment"
+            }
+          }
+        ],
+        "children": [
+          {
+            "type": "Text",
+            "value": "increment"
+          }
+        ]
+      },
+      {
+        "type": "Element",
+        "name": "p",
+        "attributes": [],
+        "children": [
+          {
+            "type": "Text",
+            "value": "Count is"
+          },
+          {
+            "type": "Expression",
+            "value": "counter"
+          }
+        ]
+      },
+      {
+        "type": "Element",
+        "name": "button",
+        "attributes": [
+          {
+            "type": "Attribute",
+            "name": "on:click",
+            "value": {
+              "type": "Expression",
+              "value": "decrement"
+            }
+          }
+        ],
+        "children": [
+          {
+            "type": "Text",
+            "value": "decrement"
+          }
+        ]
+      }
+    ]
+  }
+}
