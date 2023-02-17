4a719125a74127906b7af464ee3b545152be0e49
suraj0901 <66919989+suraj0901@users.noreply.github.com>
2023-02-17 12:48:05 +0530

    2 files modified

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,11 @@ const app = () => {
   const [counter, setCounter] = useState(0);
   const increment = () => setCounter((prev) => prev++);
   const decrement = () => setCounter((prev) => prev--);
+  const src = "./image"
   return (
     <div>
       <button on:click={increment}>increment</button>
+      <img {src} alt="random image"/>
       <p>Count is {counter()}</p>
       <button on:click={decrement}>decrement</button>
     </div>
