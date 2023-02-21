1800f2c7330eef86ca48b3bc052c0bb453b33565
suraj0901 <66919989+suraj0901@users.noreply.github.com>
2023-02-21 15:18:44 +0530

    3 files modified

--- public/app.js
+++ public/app.js
@@ -1,31 +1,28 @@
+export const _temp$ = _runtime$.template(
+  '<button >increment</button> <img  alt="random image"/> <p >Count is <!></p> <p ><!> * 2 = <!></p> <button >decrement</button>'
+);
 const app = () => {
   const [counter, setCounter] = useState(0);
   const increment = () => setCounter((prev) => prev++);
   const decrement = () => setCounter((prev) => prev--);
   const src = './image';
   return (() => {
-    const _temp$ = _runtime$.template(
-      '<button >increment</button> <img  alt="random image"/> <p >Count is <!></p> <p ><!> * 2 = <!></p> <button >decrement</button>'
-    );
-
-    return () => {
-      let _el$ = _temp$.cloneNode(true);
-      _el$.firstChild.$$click = increment;
-      _runtime$.bind(() =>
-        _el$.firstChild.nextSibling.setAttribute('src', src)
-      );
-      _el$.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.$$click =
-        decrement;
-      _runtime$.replace([
-        _el$.firstChild.nextSibling.nextSibling.firstChild.nextSibling,
-        () => counter(),
-        _el$.firstChild.nextSibling.nextSibling.nextSibling.firstChild,
-        () => counter(),
-        _el$.firstChild.nextSibling.nextSibling.nextSibling.firstChild
-          .nextSibling.nextSibling,
-        () => counter() * 2,
-      ]);
-      return _el$;
-    };
+    let _el$ = _temp$.cloneNode(true);
+    _el$.firstChild.$$click = increment;
+    _runtime$.bind(() => _el$.firstChild.nextSibling.setAttribute('src', src));
+    _el$.firstChild.nextSibling.nextSibling.nextSibling.nextSibling.$$click =
+      decrement;
+    _runtime$.replace([
+      _el$.firstChild.nextSibling.nextSibling.firstChild.nextSibling,
+      () => counter(),
+      _el$.firstChild.nextSibling.nextSibling.nextSibling.firstChild,
+      () => counter(),
+      _el$.firstChild.nextSibling.nextSibling.nextSibling.firstChild.nextSibling
+        .nextSibling,
+      () => counter() * 2,
+    ]);
+    return _el$;
   })();
 };
+
+export default app;
--- src/app.jsx
+++ src/app.jsx
@@ -13,3 +13,5 @@ const app = () => {
     </>
   );
 };
+
+export default app
