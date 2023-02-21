27315f1310cade577717382cd3ae38e3fb326602
suraj0901 <66919989+suraj0901@users.noreply.github.com>
2023-02-21 11:56:11 +0530

    2 files modified

--- public/app.js
+++ public/app.js
@@ -1,17 +1,25 @@
-(() => {
-  const _temp$ = _runtime$.template(
-    '<button >increment</button> <img  alt=random image/> <p >Count is <!></p> <button >decrement</button>'
-  );
-
-  return () => {
-    let _el$ = _temp$.cloneNode(true);
-    _el$.firstChild.$$click = increment;
-    _runtime$.bind(() => _el$.firstChild.nextSibling.setAttribute(src, src));
-    _runtime$.replace(
-      _el$.firstChild.nextSibling.nextSibling.firstChild.nextSibling,
-      () => counter()
+const app = () => {
+  const [counter, setCounter] = useState(0);
+  const increment = () => setCounter((prev) => prev++);
+  const decrement = () => setCounter((prev) => prev--);
+  const src = './image';
+  return (() => {
+    const _temp$ = _runtime$.template(
+      '<button >increment</button> <img  alt="random image"/> <p >Count is <!></p> <button >decrement</button>'
     );
-    _el$.firstChild.nextSibling.nextSibling.nextSibling.$$click = decrement;
-    return _el$;
-  };
-})();
+
+    return () => {
+      let _el$ = _temp$.uundefineddefinedloneNode(true);
+      _el$.firstChild.$$click = increment;
+      _runtime$.bind(() =>
+        _el$.firstChild.nextSibling.setAttribute('src', src)
+      );
+      _runtime$.replace(
+        _el$.firstChild.nextSibling.nextSibling.firstChild.nextSibling,
+        () => counter()
+      );
+      _el$.firstChild.nextSibling.nextSibling.nextSibling.$$click = decrement;
+      return _el$;
+    };
+  })();
+};
